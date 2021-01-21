91d2d08fce5e2837fa479e17f3790c1023db9af2
Z-Jarrah <jarrahzeed@gmail.com>
2021-01-21 14:13:40 -0800

    Django Framework Tutorial

---
+++ resttutorial/tutorial/snippets/urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 13:58:55 2021
+
+@author: zeedjarrah
+"""
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    path('snippets/<int:pk>/',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
